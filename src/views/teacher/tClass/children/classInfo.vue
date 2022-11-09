b8df8e4bc7eb6f96d6ebe7f9247445258fa88450
wxyzb <2529140864@qq.com>
2022-11-09 21:57:00 +0800

    班级管理

---
+++ src/views/teacher/tClass/children/classInfo.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <!-- 班级信息 -->
+    <div class="classInfo">
+      <span><strong>班级名称：</strong>{{classInfo.name}}</span>
+      <span><strong>班级描述：</strong>{{classInfo.major}}</span>
+      <span><strong>任课老师：</strong>{{teacherInfo.name}}</span>
+    </div>
+
+    <!-- 学生信息 -->
+    <div class="studentBox">
+      <h3 class="title">学生列表</h3>
+      <el-button type="primary" size="small" class="addStuBtn">添加学生</el-button>
+      <el-table
+        :data="studentList"
+        stripe
+        style="width: 100%">
+        <el-table-column type="index" label="序号" width="60px"></el-table-column>
+        <el-table-column prop="name" label="学生姓名"></el-table-column>
+        <el-table-column prop="studentId" label="学号"></el-table-column>
+        <el-table-column label="操作" width="60px">
+          <el-button type="danger" size="mini">删除</el-button>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'classInfo',
+  data() {
+    return {
+      // 班级信息
+      classInfo:{
+        class_id: '1',
+        name: '2001班',
+        major: '计算机科学与技术专业' 
+      },
+      // 教师信息
+      teacherInfo: {
+        teacher_id: '1',
+        name: '王肖'
+      },
+      // 学生列表
+      studentList: [
+        {
+          studentId: '2001',
+          name: '张三'
+        },
+        {
+          studentId: '2002',
+          name: '李四'
+        },
+        {
+          studentId: '2003',
+          name: '王五'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  // 班级信息
+  .classInfo {
+    display: flex;
+    flex-direction: column;
+    height: 100px;
+    margin: 20px 0;
+    span:nth-child(2) {
+      margin: 16px 0;
+    }
+  }
+
+  // 学生信息
+  .studentBox {
+    width: 100%;
+    height: calc(100vh - 120px);
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 4px;
+    .title {
+      font-size: 20px;
+      font-weight: 500;
+    }
+    .addStuBtn {
+      margin: 16px 0;
+    }
+  }
+</style>
