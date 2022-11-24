42d4b70608ee6d35fd5df479008456ab16bb33dc
nsx <1743091664@qq.com>
2022-11-24 21:26:10 +0800

    内容

---
+++ src/components/common/TopBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="top-bar">
+    <div class="title wrap-v1">
+      <slot>
+        默认标题
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+.top-bar {
+  width: 100%;
+  height: 40px;
+  background-color: #4498ee;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  left: 0;
+
+  .title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+  }
+}
+</style>
---
+++ src/views/student/sHome/children/sTask.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="task">
+    <TopBar>
+      作业
+    </TopBar>
+    <h2>task</h2>
+  </div>
+</template>
+
+<script >
+import TopBar from '@/components/common/TopBar.vue';
+
+export default {
+  name: "sTask",
+  components: { TopBar },
+  mounted() {
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.task {
+  display: flex;
+}
+</style>
