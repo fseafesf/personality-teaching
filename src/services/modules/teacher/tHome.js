a158389523f638e4b45746f89abc7947ef7dde0f
nsx <1743091664@qq.com>
2022-12-01 11:59:22 +0800

    内容

---
+++ src/services/modules/teacher/tHome.js
@@ -0,0 +1,16 @@
+import ptRequest from '../../request'
+
+export function login(data) {
+  return ptRequest.post({
+    url: '/teacher/login',
+    data
+  })
+}
+
+export function changePwd(data) {
+  return ptRequest.put({
+    url: '/pwd',
+    data
+  })
+}
+
